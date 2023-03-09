fc59e7be8436ad5b3fdb70fdeb4b9f3e6c245c40
Priyanka Parate <priyanka.parate@premfina.com>
2023-03-09 22:04:04 +0000

    basic commection of mongoDB with data save

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const mongoURI = 'mongodb://127.0.0.1:27017/'//as here db name is not mentioned, mongo will create default db test
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI)
+    console.log('connected to mongoDB successfully')
+}
+
+module.exports = connectToMongo
