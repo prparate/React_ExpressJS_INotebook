03f6db139338ec2d921d809f90c7eb0c4f9999ee
Priyanka Parate <priyanka.parate@premfina.com>
2023-03-10 00:51:17 +0000

    added npm bcrypt(hash,salt,pepper), jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Harryisagoodb$oy'
 
 //Create a user using: Post "api/auth/createUser". No login required
 router.post('/createUser',
@@ -24,16 +28,26 @@ router.post('/createUser',
             if (user) {
                 return res.status(400).json({ error: 'User with this email already exist' });
             }
+            const salt = await bcrypt.genSalt(10)
+            const secretPassword = await bcrypt.hash(req.body.password, salt)
+
             user = await User.create({
                 name: req.body.name,
-                password: req.body.password,
+                password: secretPassword,
                 email: req.body.email,
             })
-            res.json(user)
+
+            const data = {
+                user :{
+                    id : user.id
+                }
+            }
+            var authToken = jwt.sign(data, JWT_SECRET)
+            res.json({authToken})
 
         }catch (error) {
             console.error(error.message)
-            res.status(500).send('Some error occurred')
+            res.status(500).send('Internal Server Error')
         }
     },
 )
